6910533f4b50e428863aad5f17453b3183320caf
joeychuavertex <joey.chua@vertexholdings.com>
2022-12-12 11:28:57 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import openai
+import streamlit as st
+
+openai.api_key = st.secrets["x_api_key"]
+
+st.title("Chatbot Example")
+
+prompt = st.text_input("Enter your message:")
+
+if prompt:
+    response = openai.Completion.create(
+        # engine="text-davinci-002",
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=1024
+    )
+    st.write("Bot:", response["choices"][0]["text"])
