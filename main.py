26c646a38496ec51616e00975d7237406beea494
joeychuavertex <joey.chua@vertexholdings.com>
2022-12-17 13:07:29 +0800

    Reformat file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import pickle
 import openai
 import streamlit as st
 
 openai.api_key = st.secrets["openai_api_key"]
 model_engine = "text-davinci-002"
 
+# Define a function to fine-tune the GPT-3 model
 def fine_tune_model(question, answer):
     prompt = (question, answer)
 
@@ -19,9 +21,15 @@ def fine_tune_model(question, answer):
     return model_engine
 
 
-def generate_response(prompt, model_engine):
-    completions = openai.Completion.create(
-        engine=model_engine,
+# Define a function to save the model to a file
+def save_model(model, model_filename):
+    with open("models/{}.pkl".format(model_filename), "wb") as f:
+        pickle.dump(model, f)
+
+
+# Define a function to generate a response from the model
+def generate_response(prompt, model):
+    completions = model.completions(
         prompt=prompt,
         max_tokens=1024,
         n=1,
@@ -32,39 +40,25 @@ def generate_response(prompt, model_engine):
     message = completions.choices[0].text
     return message
 
-st.title("Chatbot using Davinci")
 
+# Create widgets for the user to input a question and an answer
 question = st.text_input("Enter a question")
 answer = st.text_area("Enter an answer")
 
+# Add a button for the user to fine-tune the model
 if st.button("Fine-tune model"):
     model_engine = fine_tune_model(question, answer)
     st.success("Model fine-tuned!")
 
-if st.button("Generate response"):
-    response = generate_response(question, model_engine)
-    st.success(response)
+# Add a widget for the user to input the name of the model file
+model_filename = st.text_input("Enter a name for the model file")
 
-
-# prompt = st.text_input("Enter your message:")
-#
-# if prompt:
-#     response = openai.Completion.create(
-#         engine="davinci",
-#         prompt=prompt,
-#         max_tokens=1024
-#     )
-#     st.write("Bot:", response["choices"][0]["text"])
-#
-#
-# st.title("Chatbot using Davinci-002")
-#
-# prompt = st.text_input("Enter your message:")
-#
-# if prompt:
-#     response = openai.Completion.create(
-#         engine="text-davinci-002",
-#         prompt=prompt,
-#         max_tokens=1024
-#     )
-#     st.write("Bot:", response["choices"][0]["text"])
+# Add a button for the user to save the trained model as a pickle file
+if st.button("Save model"):
+    model = openai.Model.create(
+        engine=model_engine,
+        name=model_filename,
+    )
+    model.wait_until_ready()
+    save_model(model, model_filename)
+    st.success("Model saved!")
